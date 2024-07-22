d6cf4a99319241b5378ea0cba58bf9ce82c4b03b
Wong Jun Onn <wongjo-wm23@student.tarc.edu.my>
2024-07-22 18:07:03 +0800

    init

---
+++ dist/javadoc/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"com.solution.inventory","l":"Database"},{"p":"com.solution.inventory","l":"Main"}];updateSearchResults();
---
+++ src/com/solution/inventory/Main.java
@@ -0,0 +1,8 @@
+package com.solution.inventory;
+
+public class Main{
+    public static void main(String[] args){
+
+        Database db = new Database();
+    }
+}
