f6f5626e200b9f0646abd775a2cb2aa026f80681
Wong Jun Onn <wongjo-wm23@student.tarc.edu.my>
2024-07-25 22:13:01 +0800

    New Database Feature

--- src/com/inventory/Database.java
+++ src/com/inventory/Database.java
@@ -4,32 +4,16 @@ import java.sql.*;
 import java.util.ArrayList;
 
 public class Database {
+    /*
+    To achieve CRUD action. For this system, create are not required.
+    * */
     private Connection con;
     private String tablename;
     public final String all = "*";
+    public final String spliter = " \\| " ;
     private ResultSet result = null;
 
     public Database() {
-    }
-
-    public Database(String tablename) {
-        this.tablename = tablename;
-    }
-
-    public void execute(String sql, String[] parameter){
-        try {
-            PreparedStatement stmt = con.prepareStatement(sql);
-            for (int i = 0; i < parameter.length; i++) {
-                stmt.setString(i + 1, parameter[i]);
-            }
-            stmt.execute();
-            result = stmt.getResultSet();
-        }catch (SQLException e){
-            e.printStackTrace();
-        }
-    }
-
-    public void startDatabase() {
         final String dbURL = "jdbc:mysql://localhost/assignment";
         final String username = "assignment";
         final String pws = "123456";
@@ -43,6 +27,32 @@ public class Database {
         }
     }
 
+    public Database(String tablename) {
+        this();
+        this.tablename = tablename;
+    }
+
+    public void execute(String sql, String[] parameter) {
+        try {
+            PreparedStatement stmt = con.prepareStatement(sql);
+            for (int i = 0; i < parameter.length; i++) {
+                stmt.setString(i + 1, parameter[i]);
+            }
+            stmt.execute();
+            result = stmt.getResultSet();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void execute(String sql){
+        try {
+            result =  con.createStatement().executeQuery(sql);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
     public void readTable(String[] columnName, String[][] condition, String additional) {
         String sql = "SELECT ";
         String[] prmt = new String[condition.length];
@@ -51,12 +61,15 @@ public class Database {
             if (i < columnName.length - 1)
                 sql += ", ";
         }
-        sql += " FROM " + tablename + " WHERE ";
-        for (int i = 0; i < condition.length; i++) {
-            sql += condition[i][0] + " LIKE ? ";
-            prmt[i] = condition[i][1];
-            if (i < condition.length - 1)
-                sql += "AND ";
+        sql += " FROM " + tablename;
+        if(condition.length != 0) {
+            sql += " WHERE ";
+            for (int i = 0; i < condition.length; i++) {
+                sql += condition[i][0] + " LIKE ? ";
+                prmt[i] = condition[i][1];
+                if (i < condition.length - 1)
+                    sql += "AND ";
+            }
         }
         sql += additional;
         execute(sql,  prmt);
@@ -64,6 +77,10 @@ public class Database {
 
     public void readTable(String[] columnName, String[][] condition){
         readTable(columnName, condition, "");
+    }
+
+    public void readTable(String[] columnName){
+        readTable(columnName, new String[0][0] , "");
     }
 
     public String getResult(String columnName) {
@@ -118,5 +135,28 @@ public class Database {
         }
         sql += ")";
         execute(sql, values);
+    }
+
+    public void updateTable(String[][] values, String[][] condition){
+        String[] prmt = new String[condition.length + values.length];
+        String sql = "UPDATE " + tablename + " SET ";
+        for(int i = 0 ; i < values.length; i++) {
+            sql += values[i][0] + " = ? ";
+            prmt[i] = values[i][1];
+            if (i < values.length - 1) {
+                sql += ", ";
+            }
+        }
+        if(condition.length != 0){
+            sql += " WHERE ";
+            for(int i = 0 ; i < condition.length; i++){
+                sql += condition[i][0] + " LIKE ? ";
+                prmt[i + values.length] = condition[i][1];
+                if(i < condition.length - 1){
+                    sql += ", ";
+                }
+            }
+        }
+        execute(sql, prmt);
     }
 }
