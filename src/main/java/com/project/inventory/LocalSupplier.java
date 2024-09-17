097123eb4301642e95276c18bfbae486646e0a78
wongjo-wm23@student.tarc.edu.my <wongjo-wm23@student.tarc.edu.my>
2024-09-17 23:45:26 +0800

    Revert "GW"
    This reverts commit d6a39a057a05a7478b580d4c9ee34efc96c6a14d.

--- src/main/java/com/project/inventory/LocalSupplier.java
+++ src/main/java/com/project/inventory/LocalSupplier.java
@@ -2,12 +2,10 @@
 package com.project.inventory;
 
 public class LocalSupplier extends Supplier {
-    private double importDuty = 0.00;
-    private String supplierType;
-    //constructor
-    public LocalSupplier(){
-    }
     
+    private static final String SUPPLIER_TYPE = "Local";
+    private double importDuty = 0.00;
+    //constructor
     public LocalSupplier(String supplierId, String supplierName, String supplierAddress, String supplierEmail){
         super(supplierId, supplierName, supplierAddress, supplierEmail);
     }
@@ -16,26 +14,11 @@ public class LocalSupplier extends Supplier {
      public double getImportDuty(){
         return importDuty;
     }
-     
-    public String getSupplierType(){
-        return supplierType;
-    }
-     
-   
-    public void setImportDuty(double importDuty){
-        this.importDuty = importDuty;
-    }
-    
-    public void setSupplierType(){
-        this.supplierType = "Local";
-    }
-      
-      
     
      
      @Override
     public String toString(){
-        return super.toString() + String.format("\nSupplier Type: %s", "Local");
+        return super.toString() + String.format("\nSupplier Type: %s", SUPPLIER_TYPE);
     }
     
     
--- src/main/java/com/project/inventory/Supplier.java
+++ src/main/java/com/project/inventory/Supplier.java
@@ -11,10 +11,6 @@ import java.util.ArrayList;
  */
 public class Supplier {
     private String supplierId, supplierName, supplierAddress, supplierEmail;
-    public static int numSupplier = 0;
-    private static Database db = new Database("supplier");
-
-  
     
     //constructor
     public Supplier(){
@@ -26,8 +22,6 @@ public class Supplier {
         this.supplierName = supplierName;
         this.supplierAddress = supplierAddress;
         this.supplierEmail = supplierEmail;
-       
- 
     }
     
     //getter
@@ -63,186 +57,13 @@ public class Supplier {
     public void setSupplierEmail(String supplierEmail){
         this.supplierEmail = supplierEmail;
     }
-    
-
- 
   
     //methods
     @Override
     public String toString(){
         return "Supplier Name: " + supplierName + "Supplier Address: " + supplierAddress + "Supplier Email:" + supplierEmail;
     }
+  
     
-    public String[][] getAllSupplierInfo(){
-        String[] columns = {"supplier_id", "supplier_name", "supplier_address", "email_address", "supplier_type", "import_duty"};
-        String[][] supplierInfo = new String[6][];
-        
-        for(int i = 0; i < columns.length; i++){
-            String[] columnName = {columns[i]};
-            
-            db.readTable(columnName);
-            
-            ArrayList<String> resultList = db.getResult();
-            
-            supplierInfo[i] = new String[resultList.size()];
-            
-            for(int j = 0; j < resultList.size(); j++){
-                supplierInfo[i][j] = resultList.get(j).replaceAll(Database.delimiter, "");
-            }
-            
-            if(i == 0)
-                numSupplier = resultList.size();
-            
-           
-        }
-        String supplierInfo2[][] = new String[supplierInfo[0].length][columns.length];
     
-        for (int i = 0; i < supplierInfo[0].length; i++) {
-           for (int j = 0; j < columns.length; j++) {
-            supplierInfo2[i][j] = supplierInfo[j][i];
-            }
-        }
-    
-        return supplierInfo2;
-    }
-   
-    public String[][] getAllSupplierInfo(String supplierId) {
-        String[] columns = {"supplier_id", "supplier_name", "supplier_address", "email_address", "supplier_type", "import_duty"};
-        Object[][] condition = {{"supplier_id", supplierId}};
-
-      
-        db.readTable(columns, condition);
-
-        
-        ArrayList<ArrayList<Object>> result = db.getObjResult();
-
-       
-        String[][] supplierInfo = new String[result.size()][columns.length];
-
-        for (int i = 0; i < result.size(); i++) {
-            for (int j = 0; j < result.get(i).size(); j++) {
-                supplierInfo[i][j] = result.get(i).get(j).toString();
-            }
-    }
-    
-    return supplierInfo;
-    }
- 
-    public void addSupplier(Supplier supplier){
-        
-        String importDutyStr, type;
-        
-        
-        numSupplier++;
-        
-        if(supplier instanceof ForeignSupplier){
-            importDutyStr = String.valueOf(((ForeignSupplier)supplier).getImportDuty());
-            type = ((ForeignSupplier)supplier).getSupplierType();
-        }else{
-            importDutyStr = String.valueOf(((LocalSupplier)supplier).getImportDuty());
-            type = ((LocalSupplier)supplier).getSupplierType();
-        }
-       
-       
-        String[] supplierColumn = {"supplier_id", "supplier_name", "supplier_address", "email_address", "supplier_type", "import_duty"};
-        String[] value = {supplier.getSupplierId(), supplier.getSupplierName(), supplier.getSupplierAddress(), supplier.getSupplierEmail(), type, importDutyStr};
-        db.insertTable(supplierColumn, value);
-        System.out.println("Added Successfully");
-    }
-    
-    public boolean validateSupplierInfo(String id, String name, String address, String email, String type){
-        
-        
-        if(!validateSupplierName(name) || !validateSupplierAddress(address) || !validateSupplierEmail(email))
-            return false;
-    
-        if(!type.equals("Local") && !type.equals("Foreign")){
-            System.out.println("Invalid Supplier Type!");
-            return false;
-        }
-       
-        return true;
-    }
-    
-    public boolean validateSupplierName(String name){
-        
-        if(name.isEmpty()|| name.isBlank() || name == null ){
-            System.out.println("Supplier Name cannot be empty!");
-            return false;
-        }    
-        if (name.length() > 50){
-            System.out.println("Error: Supplier's Name exceeds the limit of 45 characters.");
-            return false;
-        }
-        
-        return true;
-        
-    }
-    
-    public boolean validateSupplierAddress(String address){
-        String[] columns ={"supplier_address"};
-        db.readTable(columns);
-        ArrayList<String> resultList = db.getResult();
-        
-        if(address.isEmpty()|| address.isBlank() || address == null ){
-            System.out.println("Supplier Address cannot be empty!");
-            return false;
-        }
-        if (address.length() < 10 || address.length() > 100){
-            System.out.println("Error: Supplier's Address must contain 10 - 100 characters.");
-            return false;
-        }
-        for(String supplierAddress : resultList){
-            if(supplierAddress.equals(address)){
-                System.out.println("Duplicate Supplier Address");
-                return false;
-            }
-        }
-        return true;
-    }
-    
-    public boolean validateSupplierEmail(String email){
-        if(email.isEmpty()|| email.isBlank() || email == null ){
-            System.out.println("Supplier Email cannot be empty!");
-            return false;
-        }
-        if (email.length() > 45){
-            System.out.println("Error: Supplier Email exceeds the limit of 45 characters.");
-            return false;
-        }
-        
-        if(!email.contains("@") || !email.contains(".com")){
-            System.out.println("Invalid Supplier Email!");
-            return false;
-        }  
-        return true;
-    }
-    
-    public void  modifySupplier(String columnName, String supplierId, String temp){
-        boolean success;
-        Object[][] values = {{columnName, temp}};
-        
-        Object[][] condition = {{"supplier_id", supplierId}};
-        
-        success = db.updateTable(values, condition);
-        
-        if(success){
-            System.out.println("New Data Updated!");
-        }
-        else
-            System.out.println("Failed to Update New Data!");
-    }
-    
-    public void deleteSupplier(String supplierId){
-        boolean success;
-        Object[][] condition = {{"supplier_id", supplierId}};
-        
-        success = db.deleteRecord(condition);
-        
-        if(success)
-            System.out.printf("The Supplier of %s Has Been Deleted!\n", supplierId);
-        else
-            System.out.printf("Failed to Delete The Data of %s Supplier\n", supplierId);
-        
-    }
 }
